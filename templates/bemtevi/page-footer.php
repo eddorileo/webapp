4ece8c0a3addebd99200d6481ad9de099b723d11
eddorileo <dorileo@gmail.com>
2019-03-03 16:04:11 -0300

    snippet

--- templates/bemtevi/page-footer.php
+++ templates/bemtevi/page-footer.php
@@ -35,7 +35,29 @@
 <script src="/app/api.functions.js?stamp=<?php echo $cache_version; ?>"></script>
 <script src="/app/account.functions.js?stamp=<?php echo $cache_version; ?>"></script>
 <!-- END JAVASCRIPT -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-86895224-1', 'auto');
+
+  ga('send', 'pageview');
+
+if(!((localStorage.hasOwnProperty("is_partner") && localStorage.is_partner === true) || localStorage.hasOwnProperty("partner_initial_apikey")) ){
+    window.intercomSettings = {
+        app_id: 'rk27078b'
+      };
+    if(localStorage.email){
+      window.intercomSettings.email = localStorage.email 
+    }
+    if(localStorage.user_id){
+      window.intercomSettings.user_id = localStorage.user_id 
+    }
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rk27078b';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+}
+</script>
 </body>
     
 </html>
