1e5969d2fdca34be48b280b3a50dc32203e4a88e
eddorileo <dorileo@gmail.com>
2019-03-03 16:00:29 -0300

    snippet

--- templates/bemtevi/page-footer.php
+++ templates/bemtevi/page-footer.php
@@ -35,17 +35,7 @@
 <script src="/app/api.functions.js?stamp=<?php echo $cache_version; ?>"></script>
 <script src="/app/account.functions.js?stamp=<?php echo $cache_version; ?>"></script>
 <!-- END JAVASCRIPT -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-86895224-1', 'auto');
-
-  ga('send', 'pageview');
-
-</script>
 </body>
     
 </html>
