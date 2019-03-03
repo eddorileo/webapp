1ee2538162daa2651d671ee832b2ad8d01dd113d
eddorileo <dorileo@gmail.com>
2019-03-03 08:40:50 -0300

    entrar

--- templates/bemtevi/app/authentication/login/main.template.js
+++ templates/bemtevi/app/authentication/login/main.template.js
@@ -7,13 +7,13 @@ function loginPageTemplate(){
         <div class="row login-form">
             <div class="col-sm-2 col-md-2"></div>
             <div class="col-sm-8 col-md-8" id="loginbit">
-                <h2>Login | <a href="/register" class="link">Registre-se</a></h2>
+                <h2>Entrar | <a href="/register" class="link">Registrar</a></h2>
                 
-                <input id="email" type=email placeholder="your@email.com" name="email" />
+                <input id="email" type=email placeholder="seu@email.com" name="email" />
                 
                 <input id="password" type=password placeholder="Senha" name="password" />
             
-                <a href="#" id="loginButton" class="btn btn-medium">Login</a><br>
+                <a href="#" id="loginButton" class="btn btn-medium">Entrar</a><br>
                 <a href="/forgot-password"><h4>Esqueceu sua senha?</h4></a>
             </div>
                
