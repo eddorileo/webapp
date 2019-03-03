af71d1e4d2f91ba583b1beb4e0f9917190bfa3b4
eddorileo <dorileo@gmail.com>
2019-03-03 00:28:07 -0300

    as

--- app/tag/edit/functions.js
+++ app/tag/edit/functions.js
@@ -56,7 +56,7 @@ function getListTags(list_id){
             $("#newTagButton").click(function(event) {
                 $('#tagtable').append(tagTableTemplate([{
                     tag_id: "new",
-                    tag_label: "Novo R&oacute;tulo",
+                    tag_label: "Novo",
                     "colour": "ffffff",
                     "state": "live"
                 }]))
@@ -64,7 +64,7 @@ function getListTags(list_id){
                     method:'POST',
                     endpoint:'/list/'+list_id+'/tag',
                     body: {
-                        tag_label: "Novo R&oacute;tulo",
+                        tag_label: "Novo",
                         "colour": getRandomColor(),
                         "state": "live"
                     },
